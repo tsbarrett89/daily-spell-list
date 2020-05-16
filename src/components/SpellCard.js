a9af73c461db0df2c6410514610f3b2da1f6ace9
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-15 20:09:35 -0600

    onClick getSpellDetails

--- src/components/SpellCard.js
+++ src/components/SpellCard.js
@@ -4,9 +4,20 @@ import axios from 'axios'
 const SpellCard = props => {
     const [spellDetails, setSpellDetails] = useState({})
 
+    const getSpellDetails = e => {
+        e.preventDefault()
+        axios
+            .get(`https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/spells/${props.spell.index}`)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => console.log(err.response))
+    }
+
     return (
         <div>
             <h3>{props.spell.name}</h3>
+            <button onClick={getSpellDetails}>details</button>
         </div>
     )
 }
