c68bf7346a9b4df0f8eede34dcc86fceaadf7bc6
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-17 00:33:55 -0600

    set colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
 
 import { SavedSpellsContext } from './context/SavedSpellsContext'
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 const Header = () => {
 
     return (
         <header>
             <h1>Spell List Generator</h1>
-            <nav>
-                <NavLink to='/'>Spells</NavLink>
-                <NavLink to='/saved-spells'>Spell List</NavLink>
-            </nav>
         </header>
     )
 }
